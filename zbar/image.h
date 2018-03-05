b62b29c3958c60de3faa8d7df74e50f667d12d45
Peter Turczak <peter@turczak.de>
2018-03-05 12:15:54 +0100

    Add invert function for greyscale images

--- zbar/image.h
+++ zbar/image.h
@@ -63,6 +63,7 @@ struct zbar_image_s {
     const void *data;           /* image sample data */
     unsigned long datalen;      /* allocated/mapped size of data */
     void *userdata;             /* user specified data associated w/image */
+    int inverted;               /* is the picture inverted? */
 
     /* cleanup handler */
     zbar_image_cleanup_handler_t *cleanup;
@@ -98,6 +99,7 @@ typedef struct zbar_format_def_s {
 
 
 extern int _zbar_best_format(uint32_t, uint32_t*, const uint32_t*);
+extern void zbar_image_invert(const zbar_image_t *src);
 extern const zbar_format_def_t *_zbar_format_lookup(uint32_t);
 extern void _zbar_image_free(zbar_image_t*);
 
